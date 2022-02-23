678aaa93d28015f6d59fc023b1fd2408fe14732d
Subhranshu Pati <subhranshupati0412@gmail.com>
2022-02-23 23:15:36 +0530

    Create Tour | post

--- app.js
+++ app.js
@@ -1,19 +1,42 @@
+const fs= require('fs')
 const express=require('express')
 const app=express()
-app.get('/',(req,res)=>{
+
+app.use(express.json())
+
+// app.get('/',(req,res)=>{
+//     res.status(200).json({
+//         status:'success',
+//         requestedAT:`${new Date()}`,
+//         data:`Hello from server`
+//     })
+// })
+
+// app.post('/',(req,res)=>{
+//     res.status(200).json({
+//         requestedAT:`${new Date()}`,
+//         data:`Hello from server POST`
+//     })
+// })
+
+const data=JSON.parse(fs.readFileSync(`${__dirname}\\dev-data\\data\\tours-simple.json`))
+
+app.get('/api/v1/tours',(req,res)=>{
     res.status(200).json({
         status:'success',
-        requestedAT:`${new Date()}`,
-        data:`Hello from server`
+        results:data.length,
+        data
     })
 })
 
-app.post('/',(req,res)=>{
-    res.status(200).json({
-        requestedAT:`${new Date()}`,
-        data:`Hello from server POST`
+app.post('/api/v1/tours',(req,res)=>{
+    newData=req.body
+    res.status(newData?200:400).json({
+        status:`${newData?200:400}`,
+        newData
     })
 })
+
 
 const port=3000
 app.listen(3000,()=>{
